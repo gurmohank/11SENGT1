a1ff1912382c380be73f544844d39b6d0bba8b2e
gurmohank <gurmohan.kaur@education.nsw.gov.au>
2025-03-11 12:12:39 +1100

    Update fundamentalmathematics.py

---
+++ fundamentalmathematics.py
@@ -0,0 +1,14 @@
+#creates window
+import tkinter as tk
+root = tk.Tk()
+
+#set some window properties
+root.title("Math Buddies") #window title
+
+#set background colour by using hex code
+yellowhappy="#ffcc33" 
+root.configure(background=yellowhappy)
+
+#bg = PhotoImage(file = "Your_image.png") 
+
+root.mainloop()
