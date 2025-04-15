51a30a8c8e822830465a6f7ec2e69ad935bc8d5f
gurmohank <gurmohan.kaur@education.nsw.gov.au>
2025-04-15 22:59:32 +1000

    tried to sort out system appearance based visuals

--- fundamentalmathematics.py
+++ fundamentalmathematics.py
@@ -5,12 +5,37 @@ from PIL import Image
 app_home = ctk.CTk()
 app_home.title("Math Magic") #window title
 
+#specific colours
 lightbg = "#f1e5d2"
-app_home.config(bg=lightbg)
+darkbg = "#000000"
 
-bg1 = ctk.CTkImage(Image.open("bg_light.png"), size=(800, 600))
-lightbgimg = ctk.CTkLabel(app_home, text="", image=bg1)
-lightbgimg.pack()
+appearance_mode = ctk.get_appearance_mode()
+
+#default visuals based on systems current mode
+if appearance_mode == "light":
+    app_home.config(bg=lightbg)
+    bg_image = "bg_light.png"
+    setting_icon_image = "settings_icon_light.png"
+    label_bg_col = lightbg
+else:
+    app_home.config(bg=darkbg)
+    bg_image = "bg_dark.png"
+    setting_icon_image = "settings_icon_dark.png"
+    label_bg_col = darkbg
+
+
+#all images and icons used
+bg = ctk.CTkImage(Image.open(bg_image),size=(800, 600))
+settingicon = ctk.CTkImage(Image.open(setting_icon_image), size=(100,100))
+
+
+#placing the background image (light mode)
+lightbg_label = ctk.CTkLabel(app_home, text="", image=bg)
+lightbg_label.place(x=300, y=0)
+
+#placing settings icon
+settingicon_label = ctk.CTkLabel(app_home, text="", image=settingicon, bg_color=label_bg_col)
+settingicon_label.place(x=0, y=0)
 
 #run the application
 app_home.mainloop()
